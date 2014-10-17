b86fa9b5e89de7a2edfbb9493bfc9df05da3a768
David Park <davidpark@Davids-MacBook-Pro-4.local>
2014-10-16 23:02:27 -0500

    Initial Commit

---
+++ UITableViewControllerChallengeSolution/DPViewController.h
@@ -0,0 +1,13 @@
+//
+//  DPViewController.h
+//  UITableViewControllerChallengeSolution
+//
+//  Created by David Park on 10/16/14.
+//  Copyright (c) 2014 DP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DPViewController : UIViewController
+
+@end
---
+++ UITableViewControllerChallengeSolution/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UITableViewControllerChallengeSolution
+//
+//  Created by David Park on 10/16/14.
+//  Copyright (c) 2014 DP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DPAppDelegate class]));
+    }
+}
